43f3479811614dcd600e1b674c7d1e62549d214a
Guilherme Marconi <guiilhermemarconi@gmail.com>
2017-02-19 12:22:37 -0300

    Init structure

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import Header from './Header';
 import Footer from './Footer';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      api: null,
+      apiItemsPerPage: 20,
+    }
+  }
+
+  /**
+   * Request data from Pokeapi.co
+   */
+  getApiData() {}
+
+  componentDidMount() {}
+
   render() {
     return (
       <div className="App">
