bb33e52354fd1836f1b4236748725872f1942c4b
Guilherme Marconi <guiilhermemarconi@gmail.com>
2017-02-19 12:03:56 -0300

    Add Footer

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+  render() {
+    return(
+      <footer className="Footer">
+        <p className="Footer__credits">
+          Made with ❤️
+        </p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
